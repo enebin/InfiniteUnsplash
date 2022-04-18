85225ae4d3d84470ef5233712ec0769f4eab3a19
enebin <lyb35548@gmail.com>
2022-04-18 11:57:30 +0900

    Update comments

--- InfiniteUnsplash/Managers/CacheManager.swift
+++ InfiniteUnsplash/Managers/CacheManager.swift
@@ -8,15 +8,13 @@
 import Foundation
 import UIKit
 
-/// 이미지 데이터의 캐싱을 담당하는 매니저 클래스입니다
-/// 싱글톤 객체를 통해 접근이 가능합니다
 class CacheManager {
     static let shared = CacheManager()
     
     // 6MB on memory, 40MB on disk
     let cacheStorage = URLCache(memoryCapacity: 6*1024*1024, diskCapacity: 40*1024*1024, diskPath: nil)
     
-    /// 캐시에 이미지가 있는지 확인합니다. 캐시에 이미지가 존재한다면 이미지를, 그렇지 않다면 nil을 반환합니다
+    /// Check if the request has cached before. If image exists return image or just return nil.
     func getImageFromCache(request: URLRequest) -> UIImage? {
         if let cachedData = self.cacheStorage.cachedResponse(for: request) {
             let image = UIImage(data: cachedData.data)
@@ -26,7 +24,7 @@ class CacheManager {
         }
     }
     
-    /// 캐시에 이미지를 저장합니다
+    /// Store image in cache
     func storeAtCache(data: CachedURLResponse, request: URLRequest) {
         self.cacheStorage.storeCachedResponse(data, for: request)
     }
